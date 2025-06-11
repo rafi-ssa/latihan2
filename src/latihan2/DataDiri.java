fda36468c049174e2f7b73c693ce6c4e8687c9a2
rafi-ssa <0mhmmd.rafi0@gmail.com>
2025-06-11 15:23:22 +0800

    initial commit

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,11 @@
+package latihan2;
+
+public class DataDiri {
+    private String nama, kelas, npm;
+
+    public DataDiri(String nama, String kelas, String npm) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.npm = npm;
+    }
+}
