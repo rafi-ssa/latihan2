ae4ef9b3a7583ceb6dbf7432a91cb9c211166a7b
rafi-ssa <0mhmmd.rafi0@gmail.com>
2025-06-11 15:28:38 +0800

    membuat objek

---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,7 @@
+package latihan2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        DataDiri Raffi = new DataDiri("Muhammad Hendryan Raffi", "4C REG TI BJB","2310010571");
+    }
+}
